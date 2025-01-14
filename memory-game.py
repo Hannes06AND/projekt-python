548ed223b6335b539b38c3bc04c0b5c540ed88d9
Hannes Andreasson <hannes.andreasson06@skola.stenungsund.se>
2025-01-14 14:07:46 +0100

    nytt

--- memory-game.py
+++ memory-game.py
@@ -1,9 +1,16 @@
 import random
+from time import sleep
 sekvens = []
 for i in range(3):
     sekvens.append(random.randint(1, 9))
 start = input("Vill du spela? j/n: ")
+seconds = int("5")
+
 if start == "j":
     print(sekvens)
+    if seconds > 0:
+        while seconds > 0:
+            sleep(1)
+            seconds = seconds - 1 
 else: 
-    print("Kom tillbaka senare! ")
+    print("Kom tillbaka senare!")
