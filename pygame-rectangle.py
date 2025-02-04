de6b2612d28014a5e5ce92a4c3b9d04c6e616586
Hannes06AND <hannesbenjamin06@proton.me>
2025-02-04 16:09:03 +0100

    nytt

--- pygame-rectangle.py
+++ pygame-rectangle.py
@@ -18,7 +18,7 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
  
 pygame.init()
- 
+
 # Set the width and height of the screen [width, height]
 size = (700, 500)
 screen = pygame.display.set_mode(size)
@@ -33,8 +33,16 @@ clock = pygame.time.Clock()
 rect_x = 50
 rect_y = 50
 
-rect_change_x = 5
-rect_change_y = 5
+pad1_x = 5
+pad1_y = 50
+
+pad2_x = 5
+pad2_y = 50
+
+rect_change_x = 10
+rect_change_y = 10
+x_speed = 0
+y_speed = 0
  
 # -------- Main Program Loop -----------
 while not done:
@@ -44,6 +52,30 @@ while not done:
             done = True
  
     # --- Game logic should go here
+        # User pressed down on a key
+        elif event.type == pygame.KEYDOWN:
+            # Figure out if it was an arrow key. If so
+            # adjust speed.
+            if event.key == pygame.K_LEFT:
+                x_speed = -3
+            elif event.key == pygame.K_RIGHT:
+                x_speed = 3
+            elif event.key == pygame.K_UP:
+                y_speed = -3
+            elif event.key == pygame.K_DOWN:
+                y_speed = 3
+    
+        # User let up on a key
+        '''elif event.type == pygame.KEYUP:
+            # If it is an arrow key, reset vector back to zero
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_speed = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_speed = 0'''
+    
+    # Move the object according to the speed vector.
+    rect_x += x_speed
+    rect_y += y_speed
  
     # --- Screen-clearing code goes here
  
@@ -56,17 +88,20 @@ while not done:
  
     # --- Drawing code should go here
     pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
-    rect_x += rect_change_x
-    rect_y += rect_change_y
+    pygame.draw.rect(screen, GREEN, [rect_x + 10, rect_y + 10 ,30, 30])
+    # --- Drawing code for pads
+    pygame.draw.rect(screen, RED, [pad1_x, pad2_y, 5, 100])
+    pygame.draw.rect(screen, RED, [pad2_x, pad2_y, 5, 100])
+    #rect_x += rect_change_x
+    #rect_y += rect_change_y
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
     if rect_y > 450 or rect_y < 0:
-        rect_change_y = rect_change_y * -1
+        y_speed = y_speed * -1
     if rect_x > 650 or rect_x < 0:
-        rect_change_x = rect_change_x * -1
- 
+        x_speed = x_speed * -1
     # --- Limit to 60 frames per second
     clock.tick(60)
  
